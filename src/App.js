09793daecb9726356a75fde4b856828411633479
mateuslustosa <104770760+mateuslustosa@users.noreply.github.com>
2022-08-11 06:02:59 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class App extends Component {
+  render() {
+    return (
+      <>
+        <p>"Uma coisa é uma coisa, outra coisa é outra coisa"</p>
+      </>
+    );
+  }
+}
